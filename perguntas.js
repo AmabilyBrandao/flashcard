e06214a2e1406da009711444741cbe1d7754c1c6
AmabilyBrandao <amabily.brandao@escola.pr.gov.br>
2025-08-11 08:53:58 -0300

    abcdefg

--- perguntas.js
+++ perguntas.js
@@ -1,18 +1,36 @@
 criaCartao(
-    Cinema,
-     Quem é o vilão do filme Vingadores Guerra-Infinita?,
-    Thanos
+    'Cinema',
+     'Quem é o vilão do filme Vingadores Guerra-Infinita?',
+    'Thanos'
 )
 
 criaCartao(
-    anatomia,
-   Qual a parte do corpo que não cresce?,
-    olho
+    'anatomia',
+   'Qual a parte do corpo que não cresce?',
+    'olho'
 )
 
 criaCartao(
-    Pecuária,
-    Quantos ovos galinha bota por dia?,
-    1
+    'Pecuária',
+    'Quantos ovos galinha bota por dia?',
+    '1'
 )
 
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'Paris'
+)
+
+criaCartao(
+    'Educação Financeira',
+    'Qual a moeda usada no Japão?',
+    'Iene ou Yen'
+)
+
+
+criaCartao(
+    'Matemática',
+    'Fórmula para descobrir o valor da Hipotenusa',
+    'a² = b² + c²'
+)
